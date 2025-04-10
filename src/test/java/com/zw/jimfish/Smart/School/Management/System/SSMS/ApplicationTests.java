6e85bcbf8e0255b1555cb7c102ba9937cfb24c15
Munashe <munashek@afrosoft.co.zw>
2025-04-10 11:49:07 +0200

    setting the main branch

---
+++ src/test/java/com/zw/jimfish/Smart/School/Management/System/SSMS/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zw.jimfish.Smart.School.Management.System.SSMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
